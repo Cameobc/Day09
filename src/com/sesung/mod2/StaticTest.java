6a3d9ec94ef928f26401d87f262d3dd49007c131
Cameobc <dkqtk@naver.com>
2019-03-28 19:44:48 +0900

    190328

--- src/com/sesung/mod2/StaticTest.java
+++ src/com/sesung/mod2/StaticTest.java
@@ -7,10 +7,14 @@ public class StaticTest {
 	
 	public void instanceMethod() {
 		System.out.println("멤버메서드");
+		StaticTest.staticMethod();
 	}
 	
 	public static void staticMethod() {
 		System.out.println("클래스 메서드");
+//		this.instanceMethod
+		System.out.println(StaticTest.num2);
+//		System.out.println(this.num);
 	}
 
 }
