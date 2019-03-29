b61a564330b6c53356579cfd3c411cd6548bdcff
Cameobc <dkqtk@naver.com>
2019-03-29 17:02:34 +0900

    190329

--- src/com/sesung/mod2/ModMain.java
+++ src/com/sesung/mod2/ModMain.java
@@ -7,7 +7,6 @@ public class ModMain {
 		//클래스명.변수명 // 클래스명.메서드명
 		StaticTest.staticMethod();
 		System.out.println(StaticTest.num2);
-//		
 //		StaticTest st = new StaticTest();
 //		st.instanceMethod();
 		
